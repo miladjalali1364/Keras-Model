81c97a26fe8ee4da7cf08bd01ddcdfb9fa44ed9a
Milad Jalali <milad.jlali@gmail.com>
2022-03-27 10:44:27 +0430

    Import Required libraries

---
+++ Read-csv-File.py
@@ -0,0 +1,17 @@
+print ( "------------Import library at first------------" )
+
+from keras.models import Sequential
+from keras.preprocessing.text import Tokenizer, one_hot
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Embedding, convolutional, MaxPooling1D, Dense, Flatten
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from parsivar import Normalizer, spell_checker
+import pandas as pd
+import numpy as np
+import re
+
+
